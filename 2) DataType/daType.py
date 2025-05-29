a88f723628903745fb4f189a01c9b7a705c48939
Md. Moniruzzaman Tamim <97989157+MoniruzzamanTamim@users.noreply.github.com>
2025-05-29 14:19:37 +0600

    Add files via upload

---
+++ 2) DataType/daType.py
@@ -0,0 +1,25 @@
+
+is_String="Tamim"
+is_Int = 10
+is_Float =10.25
+is_Boolean = True
+is_Big = 1486894758743546546646
+is_list = ["Tarif" , 10, 10.25, True]
+is_Disk = {
+    "is_String":"Tamim",
+"is_Int":10,
+"is_Float" : 10.25,
+"is_Boolean" : True
+}
+
+is_Tuple=("Tarif" , 10, 10.25, True)
+is_Set = { 1,2,3,4, True}
+is_Complex = 10j
+is_Byte = b"Hello"
+is_None = None
+
+
+print(type(is_String), type(is_Int),type(is_Float),type(is_Boolean),type(is_Big),type(is_list),type(is_Complex),type(is_Disk),type(is_Byte),type(is_Set),type(is_Tuple),type(is_None))
+
+
+
---
+++ 2) DataType/string.py
@@ -0,0 +1,50 @@
+name = 'Hello my name is tamim , Iam a small man and TAMIM'
+print(len(name))
+#string with use index for print specific charecter 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print("..........................") #Last Theke start korar jonno -1 use hbe akhane 0 kaj korbe na
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print("..........................")
+print(name[0:0])
+print(name[0:1])
+print(name[0:2])
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])
+print("..........................")
+print(name[-1:])
+print(name[-2:])
+print(name[-3:])
+print(name[-4:])
+print(name[-5:])
+
+
+comment = 'hello he is a smart boy'
+sec_comment = 'HELLO IS T KERKJDFHDSLG/KFDG J GRTRYRTYRHTR'
+text = "He is TAMIM"
+text2 = "         He   "
+Is_alpha ="abc"
+Is_rep = "tamim"
+
+
+print("Convert Upercase: " + comment.upper())
+
+print("Convert LowerCase: " + sec_comment.lower())
+print("Convert Capitalized: " + comment.capitalize())
+print("Convert Title: " + comment.title())
+print("Convert FinD: " , text.find("A"))
+print("Convert Strip: " + text2.strip())
+print("Convert String as a List: " , "a,b,c".split(",") )
+print("Check ITS Digis: " , text.isdigit())
+print("Check letter: " , Is_alpha.isalpha())
+print( "Replace Text: " + Is_rep.replace("tamim", "TARIF"))
+
+
