e73118565dd53628c3ac5e55f676979a99dd4e31
Md. Moniruzzaman Tamim <97989157+MoniruzzamanTamim@users.noreply.github.com>
2025-05-29 14:17:14 +0600

    Add files via upload

---
+++ 1) Basic/Basic.py
@@ -0,0 +1,31 @@
+#This is Escaping OR Back Slash
+print("This Is Line 1 \n This Is Second line ")
+print("This Is Line 1 \n This Is Second line with Some more  \t space:")
+print("www.xpress.ltd\\account")
+print(r"c:\\hello\\")
+print('Hello It\'s meclear '  )
+print("HE IS \"Absullate\" Begener")
+sum= 50+58+28+  \
+  50+50
+print(sum)
+#THIS IS cONCATATION
+print('Hello' + ' my is ' + 'tamim')
+
+#This is F-String (fortamating String Literal)
+name ="Tamim"
+gpa=25
+age=2.5
+print(f"Hello, My Name Is {name} and My Age is {age} & My GPA is {gpa}, mrs. Salima is a \"Briant\" \nIt's very sweet")
+name = "Rahim"
+age = 25
+
+#This is F-String (fortamating String Literal) With Multi Line
+
+text = F"""
+My name is {name}.
+I am {age} years old.
+Welcome!
+"""
+
+print(text)
+
