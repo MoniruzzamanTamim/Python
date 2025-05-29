b39e854b47c966dbb8353aab086e61a855ce3080
Md. Moniruzzaman Tamim <97989157+MoniruzzamanTamim@users.noreply.github.com>
2025-05-29 14:21:27 +0600

    Add files via upload

---
+++ 3) Operator/AssignmentOperator.py
@@ -0,0 +1,33 @@
+# Basic assignment
+x = 10
+print("x =", x)  # Output: 10
+
+# Add and assign
+x += 5  # Equivalent to: x = x + 5
+print("x += 5 -->", x)  # Output: 15
+
+# Subtract and assign
+x -= 3  # Equivalent to: x = x - 3
+print("x -= 3 -->", x)  # Output: 12
+
+# Multiply and assign
+x *= 2  # Equivalent to: x = x * 2
+print("x *= 2 -->", x)  # Output: 24
+
+# Divide and assign
+x /= 4  # Equivalent to: x = x / 4
+print("x /= 4 -->", x)  # Output: 6.0
+
+# Modulus and assign
+x %= 5  # Equivalent to: x = x % 5
+print("x %= 5 -->", x)  # Output: 1.0
+
+# Exponent and assign
+x **= 3  # Equivalent to: x = x ** 3
+print("x **= 3 -->", x)  # Output: 1.0
+
+# Floor divide and assign
+x //= 2  # Equivalent to: x = x // 2
+print("x //= 2 -->", x)  # Output: 0.0
+
+
---
+++ 3) Operator/ComprerisionOperator.py
@@ -0,0 +1,23 @@
+
+x = 10
+y = 10 
+z = 5
+if x==y:
+ print("This IS Same DataType & Equal Number " ) #It Check DataType & Value 
+
+if x!=z:
+    print("The Number IS Not Equal")
+    
+    if x>z:
+        print("X Is a Greater Than Z")
+
+if z<x:
+    print("Z is A Less Then X")
+
+if x>=y:
+    print("x is a Greater Then OR Equal y")
+    
+if x<=y:
+    print("Y is a Greater Then OR Equal X")
+   
+
