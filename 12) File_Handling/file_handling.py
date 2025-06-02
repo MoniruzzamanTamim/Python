7e17215dcf45728a01d0e75c22dfc479ba28277b
Tamim <tamimiqbal896@gmail.com>
2025-06-02 18:50:10 +0600

    File_Handle

---
+++ 12) File_Handling/file_handling.py
@@ -0,0 +1,42 @@
+
+#Normal Read Data
+f = open("Practise/demo.txt", "rt")
+read_file = f.read()
+f.close()
+print(read_file)
+
+
+#if Readable Return True 
+f = open("Practise/demo.txt", "rt")
+read_file = f.readable()
+f.close()
+print(read_file)
+
+#1 line Read
+f = open("Practise/demo.txt", "rt")
+read_file = f.readline()
+f.close()
+print(read_file)
+
+
+#read data on list mode
+f = open("Practise/demo.txt", "rt")
+read_file = f.readlines()
+f.close()
+print(read_file)
+
+
+
+#Write Data overWring 
+
+w = open("12) File_Handling/NewFile", "wt")
+w_data = w.write("Hello, Tamim, How Are You!")
+print(w_data)
+w.close()
+
+
+
+a = open("12) File_Handling/NewFile", "at")
+a_data = a.write("\n This IS \"A\" , For Add Data With Ager Data thik rekhe ")
+a.close()
+print(a_data)
