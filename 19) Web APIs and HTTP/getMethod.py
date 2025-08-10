7b4cd3f6e40b2cc93ac52bed63ec5013205067f9
unknown <tamimiqbal896@gmail.com>
2025-08-10 17:50:08 +0600

    Testing

--- 19) Web APIs and HTTP/getMethod.py
+++ 19) Web APIs and HTTP/getMethod.py
@@ -5,12 +5,15 @@ import json # Importing the json library for handling JSON data
 # Function to fetch data from a given URL and print the JSON response
 # fetch_data function ----GET METHOD 
 def getData(url):
-    response = requests.get(url) # Making a GET request to the URL
-    if response.status_code == 200: # Checking if the request was successful    
-        print(f"Data fetched from {url}:")
-        print(json.dumps(response.json()[:10], indent=4)) # Printing the JSON response in a pretty format
-    else:
-        print(f"Failed to fetch data from {url}, Status Code: {response.status_code}")
+    try:
+        response = requests.get(url) # Making a GET request to the URL
+        if response.status_code == 200: # Checking if the request was successful    
+            print(f"Data fetched from {url}:")
+            print(json.dumps(response.json()[:10], indent=4)) # Printing the JSON response in a pretty format
+        else:
+            print(f"Failed to fetch data from {url}, Status Code: {response.status_code}")
+    except Exception as e:
+        print(e)
 # Main function to demonstrate fetching data from multiple URLs using threads
 def main():
     url = "https://jsonplaceholder.typicode.com/posts"
